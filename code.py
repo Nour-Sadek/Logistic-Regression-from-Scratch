b2e02b1960a405784a0211e15eba539662215f2f
Nour Sadek <nour.sadek@mail.utoronto.ca>
2023-07-01 19:22:53 +0300

    Finished Stage 2's code and docstring

--- code.py
+++ code.py
@@ -32,32 +32,30 @@ Set train_size=0.8 and random_state=43.
 
 """
 
+"""Stage 2: Gradient descent with MSE
 
-class CustomLogisticRegression:
+Description
 
-    def __init__(self, fit_intercept=True, l_rate=0.01, n_epoch=100):
-        self.fit_intercept = fit_intercept
-        self.l_rate = l_rate
-        self.n_epoch = n_epoch
+In this stage, we need to estimate the coef_ (weight) values by gradient descent 
+on the Mean squared error cost function. Gradient descent is an optimization 
+technique for finding the local minimum of a cost function by first-order 
+differentiating. To be precise, we're going to implement the Stochastic 
+gradient descent (SGD).
 
-    def sigmoid(self, t):
-        return 1 / (1 + np.exp(- t))
+Objectives
 
-    def predict_proba(self, row, coef_):
-        if self.fit_intercept:
-            t = np.dot(row, coef_[1:]) + np.array([coef_[0]])
-        else:
-            t = np.dot(row, coef_)
-        return self.sigmoid(t)
+1 - Implement the fit_mse method
+2 - Implement the predict method
 
+"""
 
 # Load the dataset
 data = load_breast_cancer(as_frame=True)
-X = data.data[['worst concave points', 'worst perimeter']]
+X = data.data[['worst concave points', 'worst perimeter', 'worst radius']]
 y = data.target
 
 # Standardize X
-for feature in ['worst concave points', 'worst perimeter']:
+for feature in X.columns.tolist():
     feature_mean = X[feature].mean()
     feature_std = X[feature].std()
     X[feature] = (X[feature] - feature_mean) / feature_std
@@ -65,4 +63,83 @@ for feature in ['worst concave points', 'worst perimeter']:
 # Split the datasets to training and test sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.8,
                                                     random_state=43)
+
+
+class CustomLogisticRegression:
+    """A simple logistic regression model."""
+
+    def __init__(self, fit_intercept=True, l_rate=0.01, n_epoch=100):
+        self.fit_intercept = fit_intercept
+        self.l_rate = l_rate
+        self.n_epoch = n_epoch
+        self.coef_ = None
+
+    def sigmoid(self, t):
+        """The logistic function used to transform the linear regression to
+        logistic regression is the sigmoid function."""
+        return 1 / (1 + np.exp(- t))
+
+    def predict_proba(self, row, coef_):
+        """Predict the probability that <row> belongs to Class 1, given the
+        weights <coef_> for each feature."""
+        if self.fit_intercept:
+            t = np.dot(row, coef_[1:]) + np.array([coef_[0]])
+        else:
+            t = np.dot(row, coef_)
+        return self.sigmoid(t)
+
+    def fit_mse(self, X_train, y_train):
+        """Update the <self.coef_> attribute by estimating the optimal weight
+        values using the Gradient Descent method with the Mean Squared Error
+        cost function.
+
+        We will start with all weight values being equal to zero.
+
+        """
+        if self.fit_intercept:
+            count = len(X_train.columns.tolist()) + 1
+        else:
+            count = len(X_train.columns.tolist())
+
+        # Initialize the weights
+        coef_ = np.zeros(count)
+
+        # Training loop
+        for _ in range(self.n_epoch):
+            i = 0
+            for _, row in X_train.iterrows():
+                y_hat = self.predict_proba(row, coef_)
+                # Update all weights
+                if self.fit_intercept:
+                    ind = 1
+                    for value in row:
+                        coef_[ind] = coef_[ind] - self.l_rate * (
+                                        y_hat - y_train.iloc[i]) * y_hat * (
+                                                     1 - y_hat) * value
+                        ind = ind + 1
+                    coef_[0] = coef_[0] - self.l_rate * (
+                                    y_hat - y_train.iloc[i]) * y_hat * (
+                                               1 - y_hat)
+                else:
+                    ind = 0
+                    for value in row:
+                        coef_[ind] = coef_[ind] - self.l_rate * (
+                                        y_hat - y_train.iloc[i]) * y_hat * (
+                                                     1 - y_hat) * value
+                        ind = ind + 1
+                i = i + 1
+
+        self.coef_ = coef_
+
+    def predict(self, X_test, cut_off=0.5):
+        """After the optimal weight values have been determined using the
+        self.fit_mse() method and the <self.coef_> has been updates, output the
+        prediction of whether the dataset belongs to class 1 or class 0.
+        Predictions can only take two values: 0 or 1.
+
+        """
+        predictions = self.predict_proba(X_test.to_numpy(), self.coef_)
+        predictions[predictions >= cut_off] = 1
+        predictions[predictions < cut_off] = 0
+        return predictions
 
